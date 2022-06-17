abb8cf45f4c4d04ec78e8047b214a2a73f7bff0a
Jonathan <41968795+Withoutbytes@users.noreply.github.com>
2022-06-17 01:22:08 -0300

    test

--- utils/database.ts
+++ utils/database.ts
@@ -1,5 +1,5 @@
 import { MongoClient, ServerApiVersion } from "mongodb";
-
+console.log("uri", process.env.MONGODB_URI);
 export default new MongoClient(process.env.MONGODB_URI || "", {
 	serverApi: ServerApiVersion.v1,
 });
