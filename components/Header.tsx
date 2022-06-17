efa276f917adf241caeef1be566c0d6281b315eb
Jonathan <41968795+Withoutbytes@users.noreply.github.com>
2022-06-17 11:50:59 -0300

    feat: dynamic css file

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-css-tags */
 import Head from "next/head";
 
 const Header: React.FC = () => {
@@ -6,6 +7,7 @@ const Header: React.FC = () => {
 			<title>Poc nextjs</title>
 			<meta name="description" content="Poc theme dynamic" />
 			<link rel="icon" href="/favicon.ico" />
+			<link rel="stylesheet" href="/theme.css" />
 		</Head>
 	);
 };
